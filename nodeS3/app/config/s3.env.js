8c81aea667b6d783fabe48b535e3ecc68e1e1b30
joshgryzen <58818027+joshgryzen@users.noreply.github.com>
2020-03-10 18:48:30 -0500

    Initial commit

---
+++ nodeS3/app/config/s3.env.js
@@ -0,0 +1,8 @@
+const env = {
+	AWS_ACCESS_KEY: 'AKIAS7LYRVOI7TJTDB54', // change to yours
+	AWS_SECRET_ACCESS_KEY: 'W0awZnd4UK/87YKKc/tCgrmychdYDGR3ECQUEDJe', // change to yours
+	REGION : 'us-east-2', // change to yours
+	Bucket: 'itgs-s3-bucket' // change to yours
+};
+ 
+module.exports = env;
